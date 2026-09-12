5a82ed0f5a01e6ca9aaa215a28921d7af08d9ff8
abdullah989al-hash <abdullah.989al@gmail.com>
2026-09-12 19:37:43 +0530

    Create MyDeviceAdminReceiver.kt

---
+++ app/src/main/java/com/mahek/assistant/MyDeviceAdminReceiver.kt
@@ -0,0 +1,19 @@
+package com.mahek.assistant
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyDeviceAdminReceiver : DeviceAdminReceiver() {
+
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+        Toast.makeText(context, "Mahek ko screen lock permission mil gayi", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDisabled(context: Context, intent: Intent) {
+        super.onDisabled(context, intent)
+        Toast.makeText(context, "Mahek ki screen lock permission hata di gayi", Toast.LENGTH_SHORT).show()
+    }
+}
